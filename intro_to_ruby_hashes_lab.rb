1385153b96c283144ce6fde1a1fe228e8b262bb0
LaNice Powell <lanicemp@aol.com>
2019-07-03 23:29:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,9 @@ def monopoly_with_third_tier
 {:railroads=>{
  :pieces=>4,
  :names=>
-    {},
+    {:reading_railroad=>{}
+      
+    },
  :rent_in_dollars=>
     { :one_piece_owned=>25,
       :two_pieces_owned=>50,
